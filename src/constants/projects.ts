92f718d1081cf2362c9fa7ac6749f4de6c8f090d
hyeran1216 <hran1216@khu.ac.kr>
2025-11-06 04:49:11 +0900

    fix router

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -20,7 +20,7 @@ export const PROJECTS: ProjectType[] = [
         title: 'Palmful',
         date: 'Fall 2025',
         summary: '사용자가 선택한 옵션에 따라 어울리는 꽃 조합과 디자인을 AI가 자동으로 생성하고, 디자인된 꽃다발은 아카이브에 저장할 수 있으며, 친구와 꽃다발을 공유할 수 있는 웹 서비스',
-        thumbnail: '/projects/palmful/palmful-thumbnail-1.png',
+        thumbnail: 'projects/palmful/palmful-thumbnail-1.png',
         tags: ["React", "TypeScript", "Tailwind CSS", "AWS", "Figma"],
         category: 'Personal Project'
     },
@@ -29,7 +29,7 @@ export const PROJECTS: ProjectType[] = [
         title: 'TeachTouch',
         date: 'Fall 2025',
         summary: '디지털 약자를 위한 학습 서비스',
-        thumbnail: '/projects/teachtouch/tt-thumbnail-1.png',
+        thumbnail: 'projects/teachtouch/tt-thumbnail-1.png',
         tags: ["React", "TypeScript", "AWS", "Figma"],
         category: 'Personal Project'
     },
@@ -38,7 +38,7 @@ export const PROJECTS: ProjectType[] = [
         title: 'EzStress',
         date: 'Spring 2025',
         summary: 'AI를 활용해 시스템 성능 병목 지점을 예측하고, 스트레스 테스트를 수행하며, 그 결과를 시각화하는 웹 서비스',
-        thumbnail: '/projects/ezstress/ezstress-thumbnail-2.jpg',
+        thumbnail: 'projects/ezstress/ezstress-thumbnail-2.jpg',
         tags: ["React", "TypeScript", "AWS", "Figma"],
         category: 'Personal Project'
     },
@@ -47,7 +47,7 @@ export const PROJECTS: ProjectType[] = [
         title: 'Image2Template',
         date: 'Spring 2025',
         summary: '포스터나 카드뉴스와 같은 디자인 이미지를 템플릿 형태로 변환하는 웹 기반 편집 도구',
-        thumbnail: '/projects/capstone/image2template-thumbnail.png',
+        thumbnail: 'projects/capstone/image2template-thumbnail.png',
         tags: ["Jupyter Notebook", "Python", "napari", "SAM", "LAMA"],
         category: 'Personal Project'
     },
@@ -56,7 +56,7 @@ export const PROJECTS: ProjectType[] = [
         title: 'Simter',
         date: 'Fall 2024',
         summary: '고민을 부담없이 털어놓고 케어받을 수 있는 웹 서비스',
-        thumbnail: '/projects/simter/simter-thumbnail-1.png',
+        thumbnail: 'projects/simter/simter-thumbnail-1.png',
         tags: ["React", "JavaScript", "Styled Components", "Netlify", "Figma", "Redux"],
         category: 'Personal Project'
     },
--- src/features/Home/sections/introSection.tsx
+++ src/features/Home/sections/introSection.tsx
@@ -3,7 +3,7 @@ import ArrowLink from '@/components/ArrowLink/arrowLink';
 export default function IntroSection() {
     return (
         <>
-            <h1 className="text-4xl font-bold mt-24">Hi, I'm Hyeran Kim<img src="/snow.png" alt="logo" className="w-9 h-9 inline-block ml-2 relative bottom-[0.14em]" /></h1>
+            <h1 className="text-4xl font-bold mt-24">Hi, I'm Hyeran Kim<img src="snow.png" alt="logo" className="w-9 h-9 inline-block ml-2 relative bottom-[0.14em]" /></h1>
             <p className="mt-8 mb-6 text-gray-600">I'm an undergraduate student at 
                 <ArrowLink href="http://swcon.khu.ac.kr/wordpress/" className="ml-1">
                     Kyung Hee University School of Software Convergence
