b25d39ecba201de6544534193be05cedb8d456dc
hyeran1216 <hran1216@khu.ac.kr>
2025-11-06 04:51:35 +0900

    fix router

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import App from './app/app.jsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <HashRouter basename="/portfolio-site">
       <App />
     </HashRouter>
   </StrictMode>,
